d24bf5695743379150962fc97ed1e2bca7980526
wangjie <728199993@qq.com>
2018-04-18 11:21:01 +0800

    complete entity Company.java

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -6,6 +6,45 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Table(name="company")
 public class Company {
+    @Id
+    private int id;
+    @Column(nullable = false)
+    private String companyName;
+    @Column(nullable = false)
+    private int employeesNumber;
 
+    public Company() {
+    }
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
