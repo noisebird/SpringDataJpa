cb522d3a3138e1311002c5ccdffd3794f84f1436
wangjie <728199993@qq.com>
2018-04-18 11:28:34 +0800

    complete function and test of method should_return_employee_list_when_input_page_request

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -27,6 +27,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findEmployeeWhoHasMaxSalaryById(@Param("companyId") Integer companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
+    Page<Employee> findAll(Pageable pageable);
     //5.查找**的所在的公司的公司名称
     //6.将*的名字改成*,输出这次修改影响的行数
     //7.删除姓名是*的employee
